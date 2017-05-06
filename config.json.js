e983f3b7191bd7cb5051711ab73bf42a9fbfa817
Kenney <zql254@vip.qq.com>
2017-05-06 10:42:22 +0800

    add into softcenter

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "DNSPOD动态域名解析", 
+    "home_url": "Module_dnspod.asp", 
+    "md5": "8e69b47a7b804565d849479be54c0725", 
+    "module": "dnspod", 
+    "title": "dnspod", 
+    "version": "1.0.0"
+}
