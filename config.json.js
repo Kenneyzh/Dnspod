6858efb13ca9abab47b006a4fbfded044033fba8
Kenney <zql254@vip.qq.com>
2017-05-06 12:44:55 +0800

    fix config

--- config.json.js
+++ config.json.js
@@ -1,8 +1,11 @@
 {
+    "build_date": "2017-05-06 12:37:57", 
     "description": "DNSPOD动态域名解析", 
     "home_url": "Module_dnspod.asp", 
-    "md5": "8e69b47a7b804565d849479be54c0725", 
+    "md5": "764c2dbb2768265b40c5792290a8578f", 
     "module": "dnspod", 
+    "name": "dnspod", 
+    "tar_url": "dnspod/dnspod.tar.gz", 
     "title": "dnspod", 
     "version": "1.0.0"
 }
